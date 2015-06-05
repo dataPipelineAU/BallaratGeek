2187bb51c816ed22f089355b17a7d64a75e94a02
Robert Layton <robertlayton@gmail.com>
2015-06-05 14:15:41 +1000

    Basic views and models

--- ballaratgeek/views.py
+++ ballaratgeek/views.py
@@ -9,7 +9,7 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
     try:
         one = DBSession.query(Event).filter(Event.name == 'one').first()
